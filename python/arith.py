38bb9942c5b16e54ebcf6a750cddcb07352c08a4
krishmavani1301 <23se02ml116@ppsu.ac.in>
2025-10-28 08:25:27 +0530

    Create arith.py

---
+++ python/arith.py
@@ -0,0 +1,8 @@
+num1 = 50
+num2 = 270
+
+# Add the two numbers
+sum_of_numbers = num1 + num2
+
+# Print the result using an f-string for formatted output
+print(f"The sum of {num1} and {num2} is: {sum_of_numbers}")
